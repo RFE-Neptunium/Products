78943af044f6eefb0ff59e13933a71c10df84b02
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-07-29 15:47:08 -0400

    setup db and index.js files, routes are set up, but queries are generic right now

--- server/db.js
+++ server/db.js
@@ -9,3 +9,42 @@ const config = reqire("./server/.env");
 mongoose.connect(`mongodb://localhost/Product`)
   .then(() => console.log("Database Connected"))
   .catch(err => console.log(err));
+
+  const getItems = (callback) => {
+
+    Product.products.find(/*????*/).sort({ term: 'asc' })
+      .then(results => callback(null, results))
+      .catch(err => callback(err));
+
+  };
+
+  const getItemByProductId = (callback) => {
+
+    Product.products.find(/*????*/).sort({ term: 'asc' })
+      .then(results => callback(null, results))
+      .catch(err => callback(err));
+
+  };
+
+  const getStylesByProductId = (callback) => {
+
+    Product.styles.find(/*????*/).sort({ term: 'asc' })
+      .then(results => callback(null, results))
+      .catch(err => callback(err));
+
+  };
+
+  const getRelatedItemsByProductId = (callback) => {
+
+    Product.related_items.find(/*????*/).sort({ term: 'asc' })
+      .then(results => callback(null, results))
+      .catch(err => callback(err));
+
+  };
+
+  module.exports = {
+    getItems,
+    getItemByProductId,
+    getStylesByProductId,
+    getRelatedItemsByProductId
+  }
--- server/index.js
+++ server/index.js
@@ -9,8 +9,65 @@ app.use(express.json());
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
+app.get('/products', function (req, res) {
 
+  db.getItems(function (err, data) {
 
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(data);
+    }
+
+  });
+
+});
+
+app.get(`/products/${product_id}`, function (req, res) {
+
+  db.getItemByProductId(function (err, data) {
+
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(data);
+    }
+
+  });
+
+});
+
+app.get(`/products/${product_id}/styles`, function (req, res) {
+
+  db.getStylesByProductId(function (err, data) {
+
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(data);
+    }
+
+  });
+
+});
+
+app.get(`/products/${product_id}/related`, function (req, res) {
+
+  db.getRelatedItemsByProductId(function (err, data) {
+
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(data);
+    }
+
+  });
+
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at ${process.env.PORT}`);
