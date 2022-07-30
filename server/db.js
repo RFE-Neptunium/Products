e80783bfc91e8f92812e80353d1d88a109ff844f
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-07-30 15:40:30 -0400

    Working on routes and queries

--- server/db.js
+++ server/db.js
@@ -1,56 +1,115 @@
-const mongoose = require("mongoose");
-const config = reqire("./server/.env");
+const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost/Product`)
-  .then(() => console.log("Database Connected"))
-  .catch(err => console.log(err));
+const { Schema } = mongoose;
 
-const getItems = (query, callback) => {
+mongoose
+  .connect('mongodb://localhost:27017/test')
+  .then(console.log('Connected to MongoDB...'))
+  .catch((err) => console.log(err));
 
-  db.product.find().limit(10)
-    .then(results => callback(null, results))
+const FeaturesSchema = new Schema({
+  id: Number,
+  product_id: Number,
+  feature: String,
+  value: String,
+});
+
+const PhotosSchema = new Schema({
+  id: Number,
+  styleId: Number,
+  url: String,
+  thumbnail_url: String,
+});
+
+const ProductSchema = new Schema({
+  id: Number,
+  name: String,
+  slogan: String,
+  description: String,
+  category: String,
+  default_price: String,
+});
+
+const RelatedSchema = new Schema({
+  id: Number,
+  current_product_id: Number,
+  related_product_id: Number,
+});
+
+const SkusSchema = new Schema({
+  id: Number,
+  styleId: Number,
+  size: String,
+  quantity: Number,
+});
+
+const StylesSchema = new Schema({
+  id: Number,
+  productId: Number,
+  name: String,
+  sale_price: String,
+  original_price: Number,
+  default_style: Number,
+});
+
+const Features = mongoose.model('Features', FeaturesSchema);
+const Photos = mongoose.model('Photos', PhotosSchema);
+const Product = mongoose.model('Product', ProductSchema);
+const Related = mongoose.model('Related', RelatedSchema);
+const Skus = mongoose.model('Skus', SkusSchema);
+const Styles = mongoose.model('Styles', StylesSchema);
+
+const getItems = (callback) => {
+
+  Product.find().limit(10)
+    .then(results => {
+      callback(null, results)
+    })
     .catch(err => callback(err));
 
 };
 
 const getItemByProductId = (product_id, callback) => {
 
-  db.product.aggregate([
-    { $match: { "id": { product_id } } },
-    { $lookup: { from: "features", localField: "id", foreignField: "product_id", as: "features" } }
-  ])
-    .then(results => callback(null, results))
+  Product
+    .findOne({ "id": product_id })
+    .then((product) => {
+      Features
+        .find({ "product_id": product_id })
+        .then((features) => {
+          product.features = features;
+          callback(null, product);
+        })
+        .catch(err => callback(err));
+    })
     .catch(err => callback(err));
 
 };
 
 const getStylesByProductId = (product_id, callback) => {
+  /* console.log(product_id);
 
-  db.styles.find({ "productId": 1 })
-    .then((res) => {
-      const styles = res;
-      const photos = [];
-      const skus = [];
-      styles.forEach(style => {
-        photos.push(db.photos.find({ "styleId": { style.id } }));
-        skus.push(db.skus.find({ "styleId": { style.id } }));
-      });
-    })
-    .then((styles, photos, skus) => {
-      for (let i = 0; i <= styles.length; i++) {
-        styles[i].photos = photos[i];
-        styles[i].skus = skus[i];
-      }
-      callback(null, styles);
-    })
-    .catch(err => callback(err));
-
+   Styles
+     .find({ "productId": product_id })
+     .then((styles) => {
+       styles.forEach(style => {
+         style.photos = Photos.find({ "styleId": style.id });
+         style.skus = Skus.find({ "styleId": style.id });
+       });
+     })
+     .then(() => callback(null, styles))
+     .catch(err => callback(err));
+ */
 };
 
 const getRelatedItemsByProductId = (product_id, callback) => {
 
-  db.related.find({ current_product_id: { product_id } })
-    .then(results => callback(null, results))
+  Related
+    .find({ "current_product_id": product_id })
+    .then((related) => {
+      console.log(related);
+      callback(null, related);
+    })
     .catch(err => callback(err));
 
 };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const db = require("./db.js"); // import mongoose db
+const express = require('express');
+const path = require('path');
+const db = require('./db');
 
 const app = express();
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, '/../dist')));
 app.use(express.json());
 // Serves up all static and generated assets in ../client/dist.
 // app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get('/products', function (req, res) {
 
-  db.getItems(req.query, (err, data) {
+  db.getItems((err, data) => {
 
     if (err) {
       console.log(err);
@@ -25,8 +26,9 @@ app.get('/products', function (req, res) {
 });
 
 app.get(`/products/:product_id`, function (req, res) {
+  console.log(req.params.product_id);
 
-  db.getItemByProductId(req.params.product_id, (err, data) {
+  db.getItemByProductId(req.params.product_id, (err, data) => {
 
     if (err) {
       console.log(err);
@@ -41,7 +43,7 @@ app.get(`/products/:product_id`, function (req, res) {
 
 app.get(`/products/:product_id/styles`, function (req, res) {
 
-  db.getStylesByProductId(req.params.product_id, (err, data) {
+  db.getStylesByProductId(req.params.product_id, (err, data) => {
 
     if (err) {
       console.log(err);
@@ -56,7 +58,7 @@ app.get(`/products/:product_id/styles`, function (req, res) {
 
 app.get(`/products/:product_id/related`, function (req, res) {
 
-  db.getRelatedItemsByProductId(req.params.product_id, (err, data) {
+  db.getRelatedItemsByProductId(req.params.product_id, (err, data) => {
 
     if (err) {
       console.log(err);
@@ -69,5 +71,5 @@ app.get(`/products/:product_id/related`, function (req, res) {
 
 });
 
-app.listen(process.env.PORT);
-console.log(`Listening at ${process.env.PORT}`);
+app.listen(port);
+console.log(`Listening at localhost:${port}`);
