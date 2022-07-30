378f6defefe89b85e8d6f7bc5f2a9224cbdc5d11
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-07-30 12:03:10 -0400

    Finished initial setup of queries

--- server/db.js
+++ server/db.js
@@ -1,50 +1,63 @@
 const mongoose = require("mongoose");
 const config = reqire("./server/.env");
 
-// 1. Use mongoose to establish a connection to MongoDB
-// 2. Set up any schema and models needed by the app
-// 3. Export the models
-// 4. Import the models into any modules that need them
-
 mongoose.connect(`mongodb://localhost/Product`)
   .then(() => console.log("Database Connected"))
   .catch(err => console.log(err));
 
-  const getItems = (callback) => {
+const getItems = (query, callback) => {
 
-    Product.products.find(/*????*/).sort({ term: 'asc' })
-      .then(results => callback(null, results))
-      .catch(err => callback(err));
+  db.product.find().limit(10)
+    .then(results => callback(null, results))
+    .catch(err => callback(err));
 
-  };
+};
 
-  const getItemByProductId = (callback) => {
+const getItemByProductId = (product_id, callback) => {
 
-    Product.products.find(/*????*/).sort({ term: 'asc' })
-      .then(results => callback(null, results))
-      .catch(err => callback(err));
+  db.product.aggregate([
+    { $match: { "id": { product_id } } },
+    { $lookup: { from: "features", localField: "id", foreignField: "product_id", as: "features" } }
+  ])
+    .then(results => callback(null, results))
+    .catch(err => callback(err));
 
-  };
+};
 
-  const getStylesByProductId = (callback) => {
+const getStylesByProductId = (product_id, callback) => {
 
-    Product.styles.find(/*????*/).sort({ term: 'asc' })
-      .then(results => callback(null, results))
-      .catch(err => callback(err));
+  db.styles.find({ "productId": 1 })
+    .then((res) => {
+      const styles = res;
+      const photos = [];
+      const skus = [];
+      styles.forEach(style => {
+        photos.push(db.photos.find({ "styleId": { style.id } }));
+        skus.push(db.skus.find({ "styleId": { style.id } }));
+      });
+    })
+    .then((styles, photos, skus) => {
+      for (let i = 0; i <= styles.length; i++) {
+        styles[i].photos = photos[i];
+        styles[i].skus = skus[i];
+      }
+      callback(null, styles);
+    })
+    .catch(err => callback(err));
 
-  };
+};
 
-  const getRelatedItemsByProductId = (callback) => {
+const getRelatedItemsByProductId = (product_id, callback) => {
 
-    Product.related_items.find(/*????*/).sort({ term: 'asc' })
-      .then(results => callback(null, results))
-      .catch(err => callback(err));
+  db.related.find({ current_product_id: { product_id } })
+    .then(results => callback(null, results))
+    .catch(err => callback(err));
 
-  };
+};
 
-  module.exports = {
-    getItems,
-    getItemByProductId,
-    getStylesByProductId,
-    getRelatedItemsByProductId
-  }
+module.exports = {
+  getItems,
+  getItemByProductId,
+  getStylesByProductId,
+  getRelatedItemsByProductId
+}
--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const app = express();
 
 app.use(express.json());
 // Serves up all static and generated assets in ../client/dist.
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get('/products', function (req, res) {
 
-  db.getItems(function (err, data) {
+  db.getItems(req.query, (err, data) {
 
     if (err) {
       console.log(err);
@@ -24,24 +24,9 @@ app.get('/products', function (req, res) {
 
 });
 
-app.get(`/products/${product_id}`, function (req, res) {
+app.get(`/products/:product_id`, function (req, res) {
 
-  db.getItemByProductId(function (err, data) {
-
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.status(200).send(data);
-    }
-
-  });
-
-});
-
-app.get(`/products/${product_id}/styles`, function (req, res) {
-
-  db.getStylesByProductId(function (err, data) {
+  db.getItemByProductId(req.params.product_id, (err, data) {
 
     if (err) {
       console.log(err);
@@ -54,9 +39,24 @@ app.get(`/products/${product_id}/styles`, function (req, res) {
 
 });
 
-app.get(`/products/${product_id}/related`, function (req, res) {
+app.get(`/products/:product_id/styles`, function (req, res) {
 
-  db.getRelatedItemsByProductId(function (err, data) {
+  db.getStylesByProductId(req.params.product_id, (err, data) {
+
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(data);
+    }
+
+  });
+
+});
+
+app.get(`/products/:product_id/related`, function (req, res) {
+
+  db.getRelatedItemsByProductId(req.params.product_id, (err, data) {
 
     if (err) {
       console.log(err);
